c8a45d23b70ec14ee0015cfc40bd3c79205ee4d2
Victor Groux <victorgroux99@gmail.com>
2026-09-03 15:28:01 +0200

    Change arguments -? to -h to be accurated with the subject

--- src/check.c
+++ src/check.c
@@ -9,7 +9,7 @@ Usage\n\
   ft_traceroute [--help] DESTINATION\n\
 \n\
 Options:\n\
-  -?, --help           Print this help message\n");
+  -h, --help           Print this help message\n");
 	exit(exit_code);
 }
 
@@ -18,7 +18,7 @@ void	checking_arguments(int argc, char **argv, char **host)
 	int	nb_host = 0;
 	for (int i = 1; i < argc; i++)
 	{
-		if (strcmp(argv[i], "-?") == 0 || strcmp(argv[i], "--help") == 0)
+		if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
 			print_help_and_exit(EXIT_SUCCESS);\
 		else if (strncmp(argv[i], "-", 1) == 0)
 		{
